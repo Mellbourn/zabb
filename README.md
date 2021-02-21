158c4f22003e66af2e7bcc0b5dc6c0dea8f5c09d
Klas Mellbourn <klas@mellbourn.net>
2021-02-21 19:48:30 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,76 @@
+# zabb
+
+[![License: MIT][license icon]][license]
+
+zabb - a plugin for finding z abbreviations
+
+## Command
+
+I love the [z][zoxide] command. It lets me quickly jump to my favorite directories by typing very few keys.
+But - sometimes I give in to my OCD and I wonder: _how_ few keys can I get a way with?
+
+I can experiment, try different short versions. But this is hit-and-miss, and ironically, it can mess with the ranking and change what abbreviations can be used.
+
+Presenting the solution: `zabb`
+
+`zabb` is a command that tries to figure out the shortest memorable abbreviation of a directory that is usable by `z` to unambiguously jump to that directory.
+
+Examples:
+
+```
+~ > ls -d Do*
+Documents Downloads
+~ > zabb Downloads
+d
+~ > z d
+~/Downloads > cd
+~ > zabb Documents
+doc
+~ > z doc
+~/Documents > cd
+~ > zabb -s Documents
+u
+m
+e
+~ > z u
+~ /Documents> cd
+~ > zabb -a Documents
+u
+m
+e
+t
+oc
+cu
+um
+[...]
+```
+
+Currently zabb only supports the [zoxide][zoxide] implementation of `z`. I welcome PRs to expand it to other implementations.
+
+## Installation
+
+### [zinit][zinit]
+
+This plugin is designed as a [zinit][zinit] module, but it's also
+compatible with other plugin managers.
+
+The only ZSH plugin manager solves the time-consuming init for
+`nvm`, `nodenv`, `pyenv`, `rvm`, `rbenv`, `thefuck`, `fasd`, etc,
+with its async [Turbo Mode][turbo mode].
+
+```zsh
+zinit ice wait'1' lucid
+zinit light mellbourn/zabb
+```
+
+## License
+
+The MIT License (MIT)
+
+Copyright (c) 2021 Klas Mellbourn
+
+[license icon]: https://img.shields.io/badge/License-MIT-green.svg
+[license]: https://opensource.org/licenses/MIT
+[zinit]: https://github.com/zdharma/zinit
+[turbo mode]: https://github.com/zdharma/zinit#turbo-mode-zsh--53
+[zoxide]: https://github.com/ajeetdsouza/zoxide
---
+++ zabb.plugin.zsh
@@ -0,0 +1,141 @@
+#!/usr/bin/env zsh
+
+_zabb_debug() {
+    if [ -n "$debug" ]; then
+        echo "$*"
+    fi
+}
+
+_zabb_dangerous() {
+    local strippedWhitespace=$(echo "$@" | xargs)
+    if [[ "$*" != "$strippedWhitespace" ]]; then
+        _zabb_debug "space is a bad abbreviation ($*)"
+        return 0
+    fi
+    # -i and -s cannot be arguments to zoxide query
+    if [[ "$*" == "-i" ]]; then
+        _zabb_debug "sorry, can't check for -i"
+        return 0
+    fi
+    if [[ "$*" == "-s" ]]; then
+        _zabb_debug "sorry, can't check for -s"
+        return 0
+    fi
+    if [[ "$*" == "~" ]]; then
+        _zabb_debug "tilde is a bad abbreviation"
+        return 0
+    fi
+    if [[ "$*" =~ ^"~".* ]]; then
+        _zabb_debug "starting with tilde is a bad abbreviation"
+        return 0
+    fi
+    return 4
+}
+
+_zabb_find_abbrevs() {
+    local directory="$*"
+    local basename=$(basename "$directory" | tr '[:upper:]' '[:lower:]')
+    local baseLength=${#basename}
+
+    for ((length = 1; length < baseLength + 1; length++)); do
+        local abbrevs=()
+        local endoffset=$([ -n "$shortest" ] || [ -n "$all" ] && echo $((baseLength - length + 1)) || echo 1)
+        for ((offset = 0; offset < endoffset; offset++)); do
+            local fragment="${basename:$offset:$length}"
+            if _zabb_dangerous "$fragment"; then
+                continue
+            fi
+            if ! local foundDirectory=$((eval ""$z_query" "$fragment"") 2>/dev/null); then
+                continue
+            fi
+            if [[ "$foundDirectory" == '' ]]; then
+                continue
+            fi
+            local realFoundDirectory=$(realpath "$foundDirectory")
+
+            if [[ "$realFoundDirectory" == "$directory" ]]; then
+                abbrevs+="$fragment"
+            fi
+        done
+        if [ ${#abbrevs[@]} -gt 0 ]; then
+            local abbrevs_found=true
+            # only report unique abbrevs
+            (
+                IFS=$'\n'
+                echo "${abbrevs[*]}"
+            ) | awk '!a[$0]++'
+
+            if [ -z "$all" ]; then
+                exit 0
+            fi
+        fi
+    done
+
+    if [[ "$abbrevs_found" == true ]]; then
+        exit 0
+    fi
+
+    echo "No abbreviation found for $(basename "${directory}")" 1>&2
+    exit 1
+}
+
+_zabb_help() {
+    echo 'Find the shortest abbreviations that can be used to autojump (a.k.a. "z") to the given (or current) directory'
+    echo
+    echo "By default, only abbreviations that start the same way as the directory name are returned."
+    echo "Non-contiguous, i.e. space separated, abbreviations are not looked for. So, in some fairly rare circumstances the shortest abbreviations may not be found."
+    _zabb_usage
+}
+
+_zabb_usage() {
+    echo
+    echo "USAGE:"
+    echo "  zabb [<DIRECTORY>]"
+    echo
+    echo "ARGS:"
+    echo "  <DIRECTORY>"
+    echo "      Directory to find z abbrevs for. If none is given, it defaults to current working directory"
+    echo
+    echo "FLAGS:"
+    echo "  -s  Allow abbreviations even if they do not start the same way as the directory name. (This will often find shorter abbreviations, but they may be less easy to remember)"
+    echo "  -a  List all (contiguous) abbreviations (implies -s)"
+    echo "  -h  Print help"
+}
+
+zabb() {
+    z_command=zoxide
+    if [ ! -x "$(command -v "$z_command")" ]; then
+        echo ""$0" only works if you have \""$z_command"\" installed to implement the \"z\" autojump command" 1>&2
+        exit 1
+    fi
+    z_query=""$z_command" query"
+
+    while getopts "sadh" opt; do
+        case $opt in
+        s) shortest=1 ;;
+        a) all=1 ;;
+        d) debug=1 ;;
+        h)
+            _zabb_help
+            exit 0
+            ;;
+        *)
+            _zabb_usage
+            exit 2
+            ;;
+        esac
+    done
+    shift $((OPTIND - 1))
+
+    if [ -z "$*" ]; then
+        directory=$(realpath "$PWD")
+    else
+        directory=$(realpath "$*")
+        if [ ! -d "$directory" ]; then
+            echo "$directory is not a valid, existing directory" 1>&2
+            exit 3
+        fi
+    fi
+
+    _zabb_find_abbrevs "$directory"
+}
